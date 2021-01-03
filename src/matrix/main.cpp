54e6c84e7e18a7b82316f8de559393a32cb7c0f9
The Devil <cpp.dvl@gmail.com>
2021-01-03 01:11:17 -0500

    Waypoint 3 -- OpenGL draw state

--- src/matrix/main.cpp
+++ src/matrix/main.cpp
@@ -31,7 +31,9 @@ int main() {
         return -1; 
     }
 
-
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     while (!glfwWindowShouldClose(window)){
         processInput(window);
